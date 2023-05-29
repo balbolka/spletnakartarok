2b45237e5ffbe4aabe9fab78dd3e91230f790cd0
balbolka <134529474+balbolka@users.noreply.github.com>
2023-05-29 17:53:17 +0200

    prvo nalaganje

---
+++ data/_d8_parenzanawaypoints_2.js
@@ -0,0 +1,1 @@
+var json__d8_parenzanawaypoints_2 = {"type":"FeatureCollection","name":"_d8_parenzanawaypoints_2","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
